3ab6f95a4e747fe52f5b6e4c766257f41804b873
Shmuel Tiko <shmuel.tiko@gmail.com>
2022-02-02 19:08:50 +0000

    .

---
+++ _deps/nlohmann_json_ext-subbuild/CMakeFiles/nlohmann_json_ext-populate.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nlohmann_json_ext-populate"
+  "CMakeFiles/nlohmann_json_ext-populate-complete"
+  "nlohmann_json_ext-populate-prefix/src/nlohmann_json_ext-populate-stamp/nlohmann_json_ext-populate-build"
+  "nlohmann_json_ext-populate-prefix/src/nlohmann_json_ext-populate-stamp/nlohmann_json_ext-populate-configure"
+  "nlohmann_json_ext-populate-prefix/src/nlohmann_json_ext-populate-stamp/nlohmann_json_ext-populate-download"
+  "nlohmann_json_ext-populate-prefix/src/nlohmann_json_ext-populate-stamp/nlohmann_json_ext-populate-install"
+  "nlohmann_json_ext-populate-prefix/src/nlohmann_json_ext-populate-stamp/nlohmann_json_ext-populate-mkdir"
+  "nlohmann_json_ext-populate-prefix/src/nlohmann_json_ext-populate-stamp/nlohmann_json_ext-populate-patch"
+  "nlohmann_json_ext-populate-prefix/src/nlohmann_json_ext-populate-stamp/nlohmann_json_ext-populate-test"
+  "nlohmann_json_ext-populate-prefix/src/nlohmann_json_ext-populate-stamp/nlohmann_json_ext-populate-update"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/nlohmann_json_ext-populate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ _deps/nlohmann_json_ext-subbuild/CMakeFiles/nlohmann_json_ext-populate.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for nlohmann_json_ext-populate.
+# This may be replaced when dependencies are built.
